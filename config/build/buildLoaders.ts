4fd96b5615a08960d837884b5a137af90164e76f
v.sapozhnikov <v.sapozhnikov@idaproject.com>
2025-06-06 22:56:02 +0300

    build: config style loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,34 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const tsLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     };
 
+    const styleLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+
+                options: {
+                    modules: {
+                        auto: /\.modules\./,
+                        localIdentName: isDev ? '[path][name]__[local]' : "[hash:base64:8]",
+                    },
+                },
+            },
+            "sass-loader",
+        ],
+    }
+
     return [
         tsLoader,
+        styleLoader,
     ];
 }
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -22,7 +22,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         plugins: buildPlugins(options),
 
         module: {
-            rules: buildLoaders(),
+            rules: buildLoaders(options),
         },
 
         resolve: buildResolvers(),
